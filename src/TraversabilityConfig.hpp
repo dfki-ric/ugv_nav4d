80f2e1b74c40971587b91f9afa6d2df37610522a
Arne Böckmann <arne.boeckmann@dfki.de>
2016-12-28 14:44:56 +0100

    removed unused members from TravConf

--- src/TraversabilityConfig.hpp
+++ src/TraversabilityConfig.hpp
@@ -6,23 +6,14 @@ namespace ugv_nav4d
 class TraversabilityConfig
 {
 public:
-    TraversabilityConfig(): maxStepHeight(0), maxSlope(0), robotHeight(0), robotSizeX(0), maxGapSize(0), numTraversabilityClasses(0), numNominalMeasurements(1), outliertFilterMinMeasurements(0), outliertFilterMaxStdDev(0.0), gridResolution(0.0) {};
+    TraversabilityConfig(): maxStepHeight(0), maxSlope(0), robotHeight(0), robotSizeX(0),    gridResolution(0.0) {};
+    
     double maxStepHeight;
     double maxSlope;
     double robotHeight;
     double robotSizeX;
     double robotSizeY;
-    double maxGapSize;
-    int numTraversabilityClasses;
     double slopeMetricScale;
-    /**
-        * The amount of measurements a MLS-Patch needs
-        * to get a probability of 1.0
-        * */
-    int numNominalMeasurements;
-    
-    int outliertFilterMinMeasurements;
-    double outliertFilterMaxStdDev;
     double gridResolution;
 };
 
