0ee5db646a0c51c97c11c11d508257e51034f405
Sascha Arnold <sascha.arnold@dfki.de>
2017-03-09 14:37:15 +0100

    remove all unused includes in main executable

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,21 +1,5 @@
-#include <iostream>
-#include <boost/archive/polymorphic_binary_iarchive.hpp>
-#include <motion_planning_libraries/sbpl/SbplMotionPrimitives.hpp>
-#include <motion_planning_libraries/sbpl/SbplSplineMotionPrimitives.hpp>
-#include <fstream>
-#include <backward/backward.hpp>
-#include <envire_core/items/Item.hpp>
-#include <envire_core/graph/EnvireGraph.hpp>
-
-#include "Planner.hpp"
 #include "PlannerGui.h"
-
-#include <base/geometry/Spline.hpp>
-#include <iostream>
-#include <fstream>
 #include <vizkit3d_debug_drawings/DrawingManager.h>
-
-// backward::SignalHandling sh;
 
 using namespace ::maps::grid;
 using namespace base::geometry;
