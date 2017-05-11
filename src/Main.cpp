82fd1d6cd2d79e743b86e2b67815c0fa90d44417
Arne Böckmann <arne.boeckmann@dfki.de>
2017-05-11 09:28:05 +0200

    catch exceptions in qt event loop and print backtrace

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,8 +1,36 @@
 #include <QApplication>
+#include <execinfo.h> //gcc only
 #include "PlannerGui.h"
+
+
+class Application : public QApplication {
+public:
+    Application(int& argc, char** argv) : QApplication(argc, argv) {}
+    
+    virtual bool notify(QObject *receiver, QEvent *e) 
+    {
+        try 
+        {
+            return QApplication::notify(receiver, e);
+        } catch (std::exception &ex) 
+        {
+            std::cerr << "CAUGHT exception in qt event loop:\n" <<
+            ex.what() << std::endl;
+            const int traceLen = 40;
+            void *symbols[traceLen];
+            const size_t size = backtrace(symbols, traceLen);
+            backtrace_symbols_fd(symbols, size, STDERR_FILENO);
+
+        } catch (...) {
+            std::cerr << "CAUGHT unknown exception in qt event loop" << std::endl;
+        }        
+         return false;
+     }
+};
+
 int main(int argc, char** argv)
 {
-    QApplication app(argc, argv);
+    Application app(argc, argv);
     PlannerGui gui(argc, argv);
     gui.show();
     app.exec(); 
