535d1abf652c871e638d1e42422204a7baf1b533
Janosch Machowinski <janosch.machowinski@dfki.de>
2017-06-28 10:06:45 +0200

    moved box into seperated header
    orogen...

--- src/AreaExplorer.hpp
+++ src/AreaExplorer.hpp
@@ -1,27 +1,12 @@
 #pragma once
 #include <memory>
-#include <base/Eigen.hpp>
 #include <base/samples/RigidBodyState.hpp>
+#include "Box.hpp"
 
 namespace ugv_nav4d 
 {
 class FrontierGenerator;
     
-
-class Box
-{
-public:
-    Box(const base::Vector3d& center, double size, const base::Quaterniond& orientation);
-    Box(const base::Vector3d& center, const base::Vector3d& dimensions, const base::Quaterniond& orientation);
-    
-    bool isInside(base::Vector3d point) const;
-    
-    base::Vector3d center;
-    base::Vector3d min;
-    base::Vector3d max;
-    base::Quaterniond orientation;
-    
-};
 
 class AreaExplorer
 {
