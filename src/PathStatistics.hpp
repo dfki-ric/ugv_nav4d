22de369b59ee62b56b07d29b2fdc2dbc98d6a9af
Arne Böckmann <arne.boeckmann@dfki.de>
2019-12-09 15:51:01 +0100

    comments

--- src/PathStatistics.hpp
+++ src/PathStatistics.hpp
@@ -6,15 +6,23 @@
 
 namespace ugv_nav4d
 {
+    
+/** Calculates statistics about different patches encountered while
+*   traversing a path.
+*   I.e. counts patchtes that the robot will traverase when following a path */
 class PathStatistic
 {
     const TraversabilityConfig &config;
 public:
+    
     class Stats
     {
         size_t obstacles;
         double minDistToObstacle;
         size_t frontiers;
+        
+        /** Minimum distance to each patch type.
+         *  Indexed by patch type. */
         std::vector<double> minDistance;
     public:
         Stats();
@@ -36,12 +44,30 @@ public:
     };
 
 protected:
+    /** Statistics about patches that the robot traverses while following a given path */
     Stats robotStats;
+    /** Statistics about patches in the corridor outside the robot bounding box */
     Stats boundaryStats;
 public:
     
     PathStatistic(const TraversabilityConfig &config);
     
+    /**
+     * Accumulates statistics about all patches that are inside a config.costFunctionDist wide corridor around @p path.
+     * This can be used to answer questions such as:
+     *  - How many obstacles does the robot traverse when following this path
+     *  - How many obstacles are close to the robot (but are not directly traversed) when following this path (boundaryStats.getNumObstacles())
+     *  - How many frontiers does the robot traverse when following this path (robotStats.getNumFrontiers())
+     *  - Is it safe to follow this path? (robotStats.getNumObstacles() == 0?)
+     *  - What is the minimum distance to an obstacle on this path?
+     *  - etc.
+     * 
+     * The statistics are used extensively during planning.
+     * 
+     * @param path List of patches that define the path.
+     * @param poses List of poses on the path. There should be one pose for each patch.
+     * @param debugObstacleName optional name of the debug drawing that should be used for this path
+     */
     void calculateStatistics(const std::vector<const TravGenNode*> &path, const std::vector<base::Pose2D> &poses, 
                              const maps::grid::TraversabilityMap3d<TravGenNode *> &trMap, const std::string &debugObstacleName = std::string());
     
