4793a7baf32b256601a4a75892cd82ef7f131f06
mulo01 <mulo01@dfki.de>
2024-06-14 15:31:11 +0000

    Update CMakeLists and add requirements for standalone build scripts

---
+++ build_standalone/requirements.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install qt5-default qttools5-dev qttools5-dev-tools
+sudo apt-get install libopenscenegraph-dev libboost-all-dev libeigen3-dev libcgal-dev libpcl-dev
