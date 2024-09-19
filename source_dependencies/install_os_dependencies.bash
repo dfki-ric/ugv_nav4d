5b7e5ed43bb80c014fb9aba9c95b96b6aecf57da
Mulo01 <mulo01@dfki.de>
2024-09-19 11:32:18 +0200

    Remove lfs as dependency

--- source_dependencies/install_os_dependencies.bash
+++ source_dependencies/install_os_dependencies.bash
@@ -3,7 +3,6 @@
 sudo apt-get update
 sudo apt-get install -y doxygen
 sudo apt-get install -y xdot
-sudo apt-get install -y git-lfs
 sudo apt-get install -y qttools5-dev qttools5-dev-tools
 sudo apt-get install -y libopenscenegraph-dev libeigen3-dev libcgal-dev libpcl-dev
 sudo apt-get install -y libboost-filesystem-dev libboost-serialization-dev libboost-system-dev 
