b272191fbff00d742026b0cb61f23fe963072006
pierrewillenbrockdfki <pierrewillenbrockdfki@users.noreply.github.com>
2025-01-22 14:27:32 +0000

    Deploying to gh-pages from @ dfki-ric/ugv_nav4d@4068552241d55975f8b718456a05d5514316eb14 🚀

--- menudata.js
+++ menudata.js
@@ -82,6 +82,7 @@ var menudata={children:[
 {text:"i",url:"functions_vars.html#index_i"},
 {text:"m",url:"functions_vars.html#index_m"},
 {text:"n",url:"functions_vars.html#index_n"},
+{text:"o",url:"functions_vars.html#index_o"},
 {text:"p",url:"functions_vars.html#index_p"},
 {text:"r",url:"functions_vars.html#index_r"},
 {text:"s",url:"functions_vars.html#index_s"},
@@ -92,7 +93,7 @@ var menudata={children:[
 {text:"Typedefs",url:"functions_type.html"},
 {text:"Enumerations",url:"functions_enum.html"},
 {text:"Enumerator",url:"functions_eval.html"},
-{text:"Related Functions",url:"functions_rela.html"}]}]},
+{text:"Related Symbols",url:"functions_rela.html"}]}]},
 {text:"Files",url:"files.html",children:[
 {text:"File List",url:"files.html"},
 {text:"File Members",url:"globals.html",children:[
