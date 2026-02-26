3d3137aec49d9ce3e15a841830899f47f80324e1
Haider <haider_lodhi@hotmail.com>
2026-02-26 08:10:40 +0100

    Update install_os_dependencies.bash

--- source_dependencies/install_os_dependencies.bash
+++ source_dependencies/install_os_dependencies.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPS="doxygen xdot qttools5-dev qttools5-dev-tools libopenscenegraph-dev libeigen3-dev libcgal-dev libpcl-dev libboost-filesystem-dev libboost-serialization-dev libboost-system-dev"
+DEPS="doxygen xdot qttools5-dev qttools5-dev-tools libopenscenegraph-dev libeigen3-dev libcgal-dev libpcl-dev libboost-filesystem-dev libboost-serialization-dev libboost-system-dev libyaml-cpp-dev"
 
 if dpkg --verify $DEPS; then
   echo "All OS dependencies are installed already. To update them run:"
